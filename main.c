784ee6e469314b0669f8bd2fc1a888d3c2c58e62
Gabriela <krustagabriela@gmail.com>
2023-06-19 17:41:11 +0200

    added libft functions and double_check

--- main.c
+++ main.c
@@ -6,18 +6,40 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 16:56:32 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/06/19 13:46:00 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/06/19 17:40:07 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
+
+/* to prevent the pointer from pointing to invalid memory after deallocation */
+void	ft_free_stack(char **argv)
+{
+	if (*argv)
+		free(*argv);
+	*argv = NULL;
+}
+
+/* is called when recieves 1 as an argument indicating an error */
+void	ft_exit(int status)
+{
+	write (1, "Error\n", 6);
+	exit (status);
+}
+
+char	**ft_fill_stack(t_stack *cur, int argc, char **argv)
+{
+	if (argc == 2)
+		cur->split = ft_split(argv[1], ' ');
+	
+}
 
 int	main(int argc, char **argv)
 {
 	t_stack	**a;
 	t_stack	**b;
 
-	a = malloc(sizeof() * );
-	b = malloc(sizeof() * );
+/* 	a = malloc(sizeof() * );
+	b = malloc(sizeof() * ); */
 
 }
--- valid_nb.c
+++ valid_nb.c
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:22:26 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/06/19 13:34:40 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/06/19 16:45:47 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,42 @@
 
 int	is_number(char *str)
 {
-	int	n;
+	long	n;
 
-	n = ft_atoi(str);
+	n = ft_long_atoi(str);
 	if (n >= INT_MIN && n <= INT_MAX)
 		return (1);
 	return (0);
 }
+/* checking if none of the numbers repeat */
+int	find_doubles(char **argv)
+{
+	int		i;
+	int		j;
+	char	*tmp;
 
-int	a_is_sorted(t_list **lst)
+	while (argv[i])
+	{
+		tmp = ft_strdup(argv[i]);
+		j = 0;
+		while (argv[i])
+		{
+			if (i == j)
+				j++;
+			else if (ft_strcmp(argv[i], argv[j]) == 0)
+			{
+				ft_free_stack(argv[i]);
+				ft_exit(1);
+			}
+			else
+				j++;
+		}
+		ft_free_stack(argv[i]);
+		i++;
+	}
+}
+
+int	a_is_sorted(t_stack **lst)
 {
 	t_list	*head;
 
