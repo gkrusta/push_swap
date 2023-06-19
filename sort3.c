c807df53c5e8809e2522c9d6f2a8775b4a787196
Gabriela <krustagabriela@gmail.com>
2023-06-19 13:46:32 +0200

    on makefile

--- sort3.c
+++ sort3.c
@@ -6,16 +6,30 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:45:20 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/06/13 15:56:58 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/06/19 13:46:32 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-int	findLargestIndex(struct ListNode* head);
+void	sort_3(t_stack **lst_a)
+{
+	t_stack	*a;
+	int	min_nb;
+	int	min_next_nb;
+
+	a = *lst_a;
+	min_nb = get_min(lst_a, );
+	min_next_nb = get_next_min(lst_a, min);
+	if (a_is_sorted(lst_a))
+		return ;
+
+}
+
+/* int	findLargestIndex(struct ListNode* head);
 {
 	while (value->next != NULL)
 	{
 
 	}
-}
+} */
