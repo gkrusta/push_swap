38a105da644f222c4e1d070c1c5c6a62c07f3e47
Gabriela <krustagabriela@gmail.com>
2023-07-06 19:38:20 +0200

    big sort divided into chunks

--- algorithm.c
+++ algorithm.c
@@ -6,34 +6,36 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 15:47:50 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/07/06 17:12:52 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/07/06 19:38:19 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-int	get_position_min(t_list *a, int index)
+int	get_max(t_list *a, int index)
 {
-	int	min_pos;
+	int		max;
 	t_list	*lst_a;
 
-	min_pos = 1;
+	max = 0;
 	lst_a = a;
 	while (lst_a != NULL)
 	{
-		if (lst_a->index < a->next->index)
-			min_pos = a->index;
+		if (lst_a->value > max)
+			max = lst_a->index;
 		lst_a = lst_a->next;
 	}
-	return (min_pos);
+	return (max);
 }
 
 int	ft_from_top(t_list **a)
 {
 	int	top;
+	int	center;
 
-	top = 1;
-	while (i >= 1)
+	top = 0;
+	center = argc / 2;
+	while (i <= center)
 	{
 		if ()
 		i++;
@@ -52,14 +54,43 @@ int	ft_from_bottom(t_list **a, int argc)
 	}
 	return (bottom);
 }
+
+void	srt_chunks(t_list **a, t_list **b, int start, int end)
+{
+	int	distance_top;
+	int	distance_bottom;
+
+	distance_top = ft_from_top(a);
+	distance_bottom = ft_from_bottom(a);
+	if (distance_top <= distance_bottom)
+	{
+		while (distance_top != 1)
+			distance_top--;
+	}
+	else
+	{
+		while (distance_bottom != argc)
+			distance_bottom--;
+	}
+}
+
 void	ft_big_sort(t_list **a, t_list **b, int argc)
 {
 	int	chunks;
 	int	start;
-	
+	int	size;
+	int	end;
+
 	if (argc <= 100)
 		chunks = 5;
 	else
 		chunks = 11;
-	devide_into_chunks(&a)
+	start = get_min(&a);
+	size = get_max(&a) / chunks;
+	while (start <= get_max(&a))
+	{
+		end = start + size;
+		srt_chunks(a, b, start, end);
+		start += size;
+	}
 }
